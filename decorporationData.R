640222eccc42ebb08dbffbaffb88f081be1f2790
Julian Rees <julianrees@users.noreply.github.com>
2017-12-04 14:49:57 -0800

    commit final commented script for publication, updated ignore

--- decorporationData.R
+++ decorporationData.R
@@ -2,8 +2,9 @@ library(ggplot2)
 library(reshape2)
 library(xlsx)
 library(plyr)
+library(superheat)
 
-# get the data from excel, trim sum column
+# get the data from excel, trim the sum column
 import_data <- read.xlsx("../FromRebecca/data/Rimport.xlsx", 
                       header = TRUE, sheetIndex = 1)
 import_data <- import_data[, -17]
@@ -15,43 +16,39 @@ import_data$Treatment <- mapvalues(import_data$Treatment, from = levels(import_d
                                      "HOPO, 1 h post", "DTPA, 1 hr post", 
                                      "HOPO, 24 h post", "HOPO, 48 h post"))
 
-# make new data for daily urine and feces
+# make new data for daily urine and feces collections
 idcols = c(1:4,17)
 import_excreta <- import_data[idcols]
 urine <- cbind(import_excreta, read.xlsx("../FromRebecca/data/Rimport.xlsx", 
                                                   header = TRUE, sheetIndex = 2)[,-1], 
                         Excretion = "Urine")
 murine <- melt(urine, id = c(colnames(urine[1:5]), "Excretion"))
-
 feces <- cbind(import_excreta, read.xlsx("../FromRebecca/data/Rimport.xlsx", 
                                          header = TRUE, sheetIndex = 3)[,-1], 
                Excretion = "Feces")
 mfeces <- melt(feces, id = c(colnames(urine[1:5]), "Excretion"))
-
 excreta <- rbind(mfeces, murine)
 colnames(excreta) <- c(colnames(excreta[1:6]), "Day", "Value")
+
+# scale data to be out of 100%
 excreta$Value <- excreta$Value * 100
-
-
 mdata <- melt(import_data, id = colnames(import_data)[idcols])
 mdata$value <- mdata$value * 100
 
-# data for Gd retention
+# build data for bodily Gd retention, scale to 100%
 reten <- cbind(import_data[,idcols], Body = rowSums(import_data[, 5:14]), 
                Excreta = rowSums(import_data[, 15:16]))
 mreten <- melt(reten, id = colnames(reten)[1:5])
 colnames(mreten) <- c(colnames(mreten)[1:5], "Location", "Value")
-
 mreten$Location <- mapvalues(mreten$Location, from = levels(mreten$Location), 
                              c("Retained", "Excreted"))
 mreten$Value <- mreten$Value * 100
 
-# data for Gd excretion
+# build data for daily Gd excretion
 totexcreta <- excreta[ which(excreta$Excretion == "Urine"), 1:7]
 totexcreta$Excretion <- "Total"
 totexcreta <- cbind(totexcreta, Value = excreta$Value[ which(excreta$Excretion == "Urine") ] + 
   excreta$Value[ which(excreta$Excretion == "Feces") ])
-
 cumexcreta <- excreta
 cumexcreta$Value[ which(cumexcreta$Day == "Day.2")] <- 
   cumexcreta$Value[ which(cumexcreta$Day == "Day.1")] + cumexcreta$Value[ which(cumexcreta$Day == "Day.2")]
@@ -59,7 +56,6 @@ cumexcreta$Value[ which(cumexcreta$Day == "Day.3")] <-
   cumexcreta$Value[ which(cumexcreta$Day == "Day.2")] + cumexcreta$Value[ which(cumexcreta$Day == "Day.3")]
 cumexcreta$Value[ which(cumexcreta$Day == "Day.4")] <- 
   cumexcreta$Value[ which(cumexcreta$Day == "Day.3")] + cumexcreta$Value[ which(cumexcreta$Day == "Day.4")]
-
 totcumexcreta <- cumexcreta[ which(cumexcreta$Excretion == "Urine"), 1:7]
 totcumexcreta$Excretion <- "Total"
 totcumexcreta <- cbind(totcumexcreta, Value = cumexcreta$Value[ which(cumexcreta$Excretion == "Urine") ] + 
@@ -113,14 +109,13 @@ p +  geom_errorbar(data = ddply(mreten[which(mreten$Location == "Retained"), ], 
 
 
 # Make individual organ retention figure
-
 # reorder organ factor to sort by contribution
 mdata$variable <- factor(mdata$variable, 
                         levels = levels(mdata$variable)[c(10,7,9,8,5,3,4,6,1,2)])
 
 ggplot(mdata [ which(mdata$variable != "Urine" & mdata$variable != "Feces"), ],
        aes(x = Treatment, y = value)) +
-  geom_jitter(aes(color = Treatment), width = 0.2) +
+  geom_jitter(aes(color = Treatment, shape = Ligand), width = 0.2) +
   #scale_fill_grey(end = 1) +
   theme(axis.text.x = element_text(angle = 35, hjust = 1)) +
   scale_y_continuous(name = expression(''^153*Gd~'Content (% RD)'), expand = c(0.02,0)) +
@@ -130,12 +125,11 @@ ggplot(mdata [ which(mdata$variable != "Urine" & mdata$variable != "Feces"), ],
   xlab(NULL) +
   theme(panel.spacing = unit(0.2, "lines")) +
   scale_color_manual(values = grouppalatte)# +
-  # ggsave(filename = '../../../Manuscripts/2017_GdDecorporation/Overleaf/allorgansjitter_preinkscape.pdf',
-  #        width = 6, height = 7, units = "in")
+  ggsave(filename = '../../../Manuscripts/2017_GdDecorporation/Overleaf/allorgansjitter_preinkscapemarks.pdf',
+         width = 6, height = 7, units = "in")
 
 
 # Make the total excretions figure
-
 totexcreta$Excretion <- as.factor(totexcreta$Excretion)
 allexcreta <- rbind(totexcreta, excreta)
 
@@ -165,7 +159,7 @@ ggplot(ddply(allcumexcreta, .(Day, Treatment, Excretion, Ligand), summarize, Ave
   geom_line(aes(color = Treatment), size = 0.7) +
   scale_x_discrete(name = "Study Day", labels = c("1", "2", "3", "4"), expand = c(0.01, 0.01)) +
   scale_y_continuous(name = expression(''^153*Gd~'Content (% RD)'), expand = c(0.05, 0.05)) +
-  geom_point(aes(color = Treatment), size = 1.7) +
+  geom_point(aes(color = Treatment, shape = Ligand), size = 1.7) +
   geom_point(data = ddply(allcumexcreta[ which(allcumexcreta$Group == "A"), ],
              .(Day, Treatment, Excretion, Ligand), summarize, Ave=mean(Value)),
              color = grouppalatte[1]) +
@@ -176,8 +170,9 @@ ggplot(ddply(allcumexcreta, .(Day, Treatment, Excretion, Ligand), summarize, Ave
   scale_color_manual(values = grouppalatte) +
   theme(panel.spacing = unit(0.6, "lines")) +
   facet_grid(Excretion ~ ., scales = "free") #+
-  # ggsave(filename = '../../../Manuscripts/2017_GdDecorporation/Overleaf/cumulativeexcretions.pdf',
-  #        width = 5.5, height = 5.5, units = "in")
+  #ggsave(filename = '../../../Manuscripts/2017_GdDecorporation/Overleaf/cumulativeexcretions_marker.pdf',
+  #       width = 5.5, height = 5.5, units = "in")
+
 
 
 #---- ! ! STATISTICAL ANALYSIS SECTION ! ! ----
@@ -185,12 +180,7 @@ library(outliers)
 library(multcomp)
 library(xtable)
 
-reten.modl <- lm(Body ~ Group, data = reten)
-summary(reten.modl)
-anova(reten.modl)
-confint(reten.modl)
-
-
+# exemplary statistical analysis of groups using Dunnett's or Tukey's tests
 fit <- aov(Body ~ Group, data = reten)
 summary(fit)
 summary(glht(fit, linfct=mcp(Group="Dunnett")))
@@ -200,167 +190,4 @@ fit <- aov(value ~ Group, data = mdata[ which(mdata$variable == "ART"), ])
 summary(fit)
 summary(glht(fit, linfct=mcp(Group="Dunnett")))
 summary(glht(fit, linfct=mcp(Group="Tukey")))
-
-
-
-# 
-# 
-# org.modl <- aov(value ~ variable, data = mdata)
-# summary(org.modl)
-# anova(org.modl)
-# TukeyHSD(org.modl)
-# summary(glht(org.modl, linfct=mcp(variable="Dunnett")))
-# 
-# fit <- aov(value ~ Treatment, data = mdata[ which(mdata$variable == "Brain"), ])
-# coef(fit)
-# summary(fit)
-# #TukeyHSD(fit)
-# summary(glht(fit, linfct=mcp(Treatment="Dunnett")))
-# summary(glht(fit, linfct=mcp(Treatment="Tukey")))
-# 
-# fit <- aov(Value ~ Treatment, data = excreta[ which(excreta$Day == "Day.1" & excreta$Excretion == "Urine"), ])
-# summary(fit)
-# summary(glht(fit, linfct=mcp(Treatment="Dunnett")))
-# 
-# fit <- aov(Value ~ Treatment+Day, data = totexcreta)
-# summary(glht(fit, linfct=mcp(Treatment="Dunnett")))
-# 
-# # are the day 2,3,4 excreta of the control group the same?
-# fit <- aov(Value ~ Day, data = totexcreta[ which(totexcreta$Group == "A"),])
-# summary(glht(fit, linfct=mcp(Day="Tukey")))
-# 
-# fit <- aov(Value ~ Day, data = excreta[ which(excreta$Group == "A" & excreta$Excretion == "Feces"),])
-# summary(glht(fit, linfct=mcp(Day="Tukey")))
-# 
-# 
-# 
-# ddply(excreta, .(Day, Excretion, Treatment), summarize, Ave=mean(Value), 
-#       Error = sd(Value)/sqrt(length(Value)))
-# 
-# 
-# ggplot(ddply(excreta[ which(excreta$Group == "A"), ],
-#              .(Day, Excretion, Treatment), summarize, Ave=mean(Value), 
-#              Error = sd(Value)/sqrt(length(Value))), 
-#        aes(x = Day, y = Ave, group = Excretion)) + 
-#   geom_point(aes(color = Excretion)) +
-#   geom_line(aes(color = Excretion)) + 
-#   #facet_wrap(Treatment)
-#   ylim(0, 3) + 
-#   geom_errorbar(aes(ymin = Ave - Error, ymax = Ave + Error), size = .4, width = 0.1) +
-#   scale_color_manual(values = grouppalatte)
-#   
-#   
-# ggplot(ddply(excreta, .(Day, Excretion, Treatment), summarize, Ave=mean(Value), 
-#              Error = sd(Value)/sqrt(length(Value))),
-#        aes(x = Day, y = Ave, group = Excretion)) + 
-#   geom_point(aes(color = Excretion)) +
-#   geom_line(aes(color = Excretion)) + 
-#   facet_wrap(~Treatment) +
-#   ylim(0, 5) + 
-#   geom_errorbar(aes(ymin = Ave - Error, ymax = Ave + Error), size = .4, width = 0.1) +
-#   scale_color_manual(values = grouppalatte)
-# 
-# 
-# 
-# 
-# fit <- aov(value ~ Treatment, data = mdata[ which(mdata$variable == "Liver"), ])
-# coef(fit)
-# #plot(fit)
-# summary(fit)
-# #TukeyHSD(fit)
-# summary(glht(fit, linfct=mcp(Treatment="Dunnett")))
-# summary(glht(fit, linfct=mcp(Treatment="Tukey")))
-# 
-# 
-# 
-# 
-# # ---- Print means and stds for table ----
-# 
-# ddply(excreta, .(Group, Treatment, Day, Excretion), summarize, Ave = mean(Value), 
-#       Error = sd(Value)/sqrt(length(Value))
-# )
-# 
-# str(totexcreta)
-# 
-# ddply(mdata[ which(mdata$variable != "Urine" & mdata$variable != "Feces"), ], 
-#       .(Group, Treatment, variable), summarize, Ave = mean(value), 
-#       Error = sd(value)/sqrt(length(value))
-# )
-# 
-# 
-# Qmdata = rm.outlier(mdata$value)
-# ddply(mdata[ which(mdata$variable == "Brain"), ], 
-#       .(Group, Treatment, variable), summarize, Ave = mean(value), 
-#       Error = sd(value)/sqrt(length(value))
-# )
-# 
-# ddply(reten, .(Treatment), summarize, Ave = mean(Body)*100, 
-#       Error = sd(Body)/sqrt(length(Body))*100)
-# ddply(reten, .(Treatment), summarize, Ave = mean(Excreta), 
-#       Error = sd(Excreta)/sqrt(length(Excreta)))
-# rmdata <- mdata
-# rmdata$variable <- with(mdata, factor(variable, levels = rev(levels(variable))))
-# 
-# 
-# ddply(rmdata[ which(rmdata$Group == "A"), ], 
-#       .(Treatment, variable), summarize, Ave = mean(value), 
-#       Err = sd(value)/sqrt(length(value)))
-# 
-# 
-# mdata[ which(mdata$Group == "C"), sum()]
-# 
-# 
-# 
-# 
-# Qmdata$value[ which(mdata$variable == "Brain" & mdata$Group == "A") ]
-# 
-# dixon.test(mdata$value[ which(mdata$variable == "Brain" & mdata$Group == "A") ], type = 0)
-# Qmdata = rm.outlier(mdata$value[ which(mdata$variable == "Brain" & mdata$Group == "A") ])
-# mean(Qmdata)
-# sd(Qmdata)/sqrt(3)
-
-# 
-# means <- dcast(mdata[ which(mdata$variable != 'NA'), ], Group+Ligand+Time ~ variable, mean)
-# sds <- dcast(mdata[ which(mdata$variable != 'NA'), ], Group+Ligand+Time ~ variable, sd)
-# 
-# emeans <- dcast(cumexcreta[ which(cumexcreta$Day == "Day.4"), ], Group+Ligand+Time ~ Excretion, mean)
-# 
-# pm <- vector(mode = 'character', length = nrow(means))
-# pm[] <- "PM"
-# 
-# 
-# 
-# round(means[4], digits = 2) 
-# round(sds[4], digits = 2)
-# paste0(round(means[,4], digits = 2), pm, round(sds[,4], digits = 2) )
-# 
-# 
-# 
-# 
-# 
-# sitab <- means[1]
-# for (i in c(4:ncol(means))){
-#   sitab <- cbind(sitab, paste0(signif(means[,i], digits = 3), pm, signif(sds[,i], digits = 2)))
-# }
-# colnames(sitab) <- colnames(means)
-# sitab <- cbind(sitab, Feces = signif(emeans$Feces, digits = 2), 
-#                Urine = round(emeans$Urine, digits = 2))
-# 
-# 
-# xtable(sitab)
-# 
-# 
-# 
-# PlusMinus(means, sds, head = colnames(means[4]), digits = 2)
-# 
-# 
-# pm <- vector(mode = 'character', length = nrow(means))
-# pm[] <- "\u00B1"
-# 
-# sds[,4]
-# 
-# xtable(means)
-# 
-# 
-
 
